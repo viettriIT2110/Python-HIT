1b5515be5272791bf22f5f0f7d63da52e3230bb0
tridungqhk3 <tridung2721@gmail.com>
2019-10-04 01:24:57 +0700

    demo nhẹ

---
+++ week 1/hello.py
@@ -0,0 +1,20 @@
+
+from turtle import *
+shape('turtle')
+for v in range(100):
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    left(9)
+
+
+# a=input('nhap so a: ')
+# b=input('nhap so b: ')
+# a = int(a)
+# b = int (b)
+# print(2*(a+b),a)
