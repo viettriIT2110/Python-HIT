e2bd41417a2818132f7ef3411d4856b02243d4af
viettriit2110 <tridung2721@gmail.com>
2019-11-09 00:21:27 +0700

    bai3

---
+++ week5/b3/b3.py
@@ -0,0 +1,11 @@
+n, arr = int(input()), {}
+for i in range(n):
+    s = input()
+    arr[s] = (1 if s not in arr else arr[s]+1)
+Max = 0
+nho = ' '
+for key in arr:
+    if Max < arr[key]:
+        Max = arr[key]
+        nho = key
+print(nho)
